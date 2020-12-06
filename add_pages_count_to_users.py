5e6e2c572f2597695b779ad614d717f3f5b2f920
euberdeveloper <euberdeveloper@gmail.com>
2020-12-06 15:29:26 +0100

    feat(add_pages): differentiate from type of event

--- add_pages_count_to_users.py
+++ add_pages_count_to_users.py
@@ -10,13 +10,21 @@ def reset_users():
     usersCollection.update_many({}, { '$unset': { 'pages_events': '' } }, upsert=False)
     print('Removed all pages_events from users', time.time())
 
+def n_pages_by_type_and_user(id, event_type):
+    return eventPagesCollection.count({ 'event_user.id': id, 'event_type': event_type })
+
 def add_tot_pages_events():
     print('Getting all non-bot users', time.time())
     users = list(usersCollection.find({ "is_bot": False }).sort('id', pymongo.ASCENDING))
     print('Adding all pages_events to users', time.time())
     for user in users:
-        n_pages = eventPagesCollection.count({ 'event_user.id': user.get('id') })
-        usersCollection.update_one({ 'id': user.get('id') }, { '$set': { 'pages_events': n_pages } })
+        update_object = dict(
+            n_pages_created = n_pages_by_type_and_user(user.get('id'), 'create'),
+            n_pages_deleted = n_pages_by_type_and_user(user.get('id'), 'delete'),
+            n_pages_moved = n_pages_by_type_and_user(user.get('id'), 'move'),
+            n_pages_restored = n_pages_by_type_and_user(user.get('id'), 'restore')
+        )
+        usersCollection.update_one({ 'id': user.get('id') }, { '$set': update_object })
     print('Added all pages_events to users', time.time())
 
 add_tot_pages_events()
