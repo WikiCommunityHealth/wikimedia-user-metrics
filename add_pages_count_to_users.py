162e8ebea19ef905cf3ebb8a32e9e74bc9acacb4
euberdeveloper <euberdeveloper@gmail.com>
2020-12-06 18:32:21 +0100

    feat: add revisions count

--- add_pages_count_to_users.py
+++ add_pages_count_to_users.py
@@ -3,6 +3,7 @@ import time
 
 client = pymongo.MongoClient()
 eventPagesCollection = client.wikimedia_history_it.pages
+eventRevisionsCollection = client.wikimedia_history_it.revisions
 usersCollection = client.wikimedia_user_metrics.users
 
 def reset_users():
@@ -12,6 +13,9 @@ def reset_users():
 
 def n_pages_by_type_and_user(id, event_type):
     return eventPagesCollection.count({ 'event_user.id': id, 'event_type': event_type })
+
+def n_revisions_by_user(id):
+    return eventRevisionsCollection.count({ 'event_user.id': id })
 
 def add_tot_pages_events():
     print('Getting all non-bot users', time.time())
@@ -27,4 +31,14 @@ def add_tot_pages_events():
         usersCollection.update_one({ 'id': user.get('id') }, { '$set': update_object })
     print('Added all pages_events to users', time.time())
 
-add_tot_pages_events()
+def add_tot_revisions_events():
+    print('Getting all non-bot users', time.time())
+    users = list(usersCollection.find({ "is_bot": False }).sort('id', pymongo.ASCENDING))
+    print('Adding all revisions_events to users', time.time())
+    for user in users:
+        update_object = dict(n_pages_edited = n_revisions_by_user(user.get('id')))
+        usersCollection.update_one({ 'id': user.get('id') }, { '$set': update_object })
+    print('Added all revisions_events to users', time.time())
+
+#add_tot_pages_events()
+add_tot_revisions_events()
