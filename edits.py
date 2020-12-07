09bda438d557ed6aa872737b39cefdd8dc2a6272
euberdeveloper <euberdeveloper@gmail.com>
2020-12-07 15:22:42 +0100

    feat(edits.py): add empty edits

--- edits.py
+++ edits.py
@@ -7,121 +7,130 @@ usersCollection = client.wikimedia_user_metrics.users
 
 PAGE_SIZE = 200000
 
-print('Starting', time.time())
-
-for i in range(0, 15):
-    print('Getting all non-bot users ids', i, time.time())
-    idsResult = list(usersCollection.aggregate([
-        {
-            '$match': {
-                'is_bot': False
-            }
-        },
-        {
-            '$sort': { 'id': 1 }
-        },
-        {
-            '$skip': i * PAGE_SIZE
-        },
-        {
-            '$limit': PAGE_SIZE
-        },
-        {
-            '$group': {
-                '_id': None,
-                'ids': {
-                    '$push': '$id'
+def add_edits():
+    print('Starting', time.time())
+    for i in range(0, 20):
+        print('Getting all non-bot users ids', i, time.time())
+        idsResult = list(usersCollection.aggregate([
+            {
+                '$match': {
+                    'is_bot': False
+                }
+            },
+            {
+                '$sort': { 'id': 1 }
+            },
+            {
+                '$skip': i * PAGE_SIZE
+            },
+            {
+                '$limit': PAGE_SIZE
+            },
+            {
+                '$group': {
+                    '_id': None,
+                    'ids': {
+                        '$push': '$id'
+                    }
                 }
             }
-        }
-    ]))
-    ids = idsResult[0]['ids']
-    print('Getted ', len(ids), 'ids')
-    print('Getted all non-bot users ids', time.time())
+        ]))
+        if (len(ids) < 1):
+            break
+        ids = idsResult[0]['ids']
+        print('Getted ', len(ids), 'ids')
+        print('Getted all non-bot users ids', time.time())
 
-    print('Getting all data from revisions', time.time())
-    results = list(eventRevisionsCollection.aggregate([
-        {
-            '$match': {
-                'event_user.id': {
-                    '$in': ids
+        print('Getting all data from revisions', time.time())
+        results = list(eventRevisionsCollection.aggregate([
+            {
+                '$match': {
+                    'event_user.id': {
+                        '$in': ids
+                    }
                 }
-            }
-        }, {
-            '$group': {
-                '_id': {
-                    'id': '$event_user.id', 
-                    'y': {
-                        '$year': '$event_timestamp'
+            }, {
+                '$group': {
+                    '_id': {
+                        'id': '$event_user.id', 
+                        'y': {
+                            '$year': '$event_timestamp'
+                        }, 
+                        'm': {
+                            '$month': '$event_timestamp'
+                        }
                     }, 
-                    'm': {
-                        '$month': '$event_timestamp'
+                    'n': {
+                        '$sum': 1
                     }
-                }, 
-                'n': {
-                    '$sum': 1
                 }
-            }
-        }, {
-            '$project': {
-                '_id': False, 
-                'id': '$_id.id', 
-                'v': [
-                    [
-                        {
-                            '$concat': [
-                                {
-                                    '$toString': '$_id.y'
-                                }, '/', {
-                                    '$toString': '$_id.m'
-                                }
-                            ]
-                        }, '$n'
+            }, {
+                '$project': {
+                    '_id': False, 
+                    'id': '$_id.id', 
+                    'v': [
+                        [
+                            {
+                                '$concat': [
+                                    {
+                                        '$toString': '$_id.y'
+                                    }, '/', {
+                                        '$toString': '$_id.m'
+                                    }
+                                ]
+                            }, '$n'
+                        ]
                     ]
-                ]
-            }
-        }, {
-            '$project': {
-                'id': 1, 
-                'v': {
-                    '$arrayToObject': '$v'
                 }
-            }
-        }, {
-            '$group': {
-                '_id': '$id', 
-                'edits': {
-                    '$mergeObjects': '$v'
+            }, {
+                '$project': {
+                    'id': 1, 
+                    'v': {
+                        '$arrayToObject': '$v'
+                    }
                 }
-            }
-        }, {
-            '$project': {
-                '_id': False,
-                'f': {
-                    'id': '$_id'
-                }, 
-                'u': {
-                    'set': {
-                        'edits': '$edits'
+            }, {
+                '$group': {
+                    '_id': '$id', 
+                    'edits': {
+                        '$mergeObjects': '$v'
+                    }
+                }
+            }, {
+                '$project': {
+                    '_id': False,
+                    'f': {
+                        'id': '$_id'
+                    }, 
+                    'u': {
+                        'set': {
+                            'edits': '$edits'
+                        }
                     }
                 }
             }
-        }
-    ]))
-    print('Getted all data from revisions', time.time())
+        ]))
+        print('Getted all data from revisions', time.time())
 
-    print('Converting all data to update queries', time.time())
-    for r in results:
-        r['u']['$set'] = r['u'].pop('set')
-    print('Converted all data to update queries', time.time())
+        print('Converting all data to update queries', time.time())
+        for r in results:
+            r['u']['$set'] = r['u'].pop('set')
+        print('Converted all data to update queries', time.time())
 
-    print('Converting all data to update queries bis', time.time())
-    results = [pymongo.UpdateOne(r['f'], r['u']) for r in results]
-    print('Getted', len(results), 'results')
-    print('Converted all data to update queries bis', time.time())
+        print('Converting all data to update queries bis', time.time())
+        results = [pymongo.UpdateOne(r['f'], r['u']) for r in results]
+        print('Getted', len(results), 'results')
+        print('Converted all data to update queries bis', time.time())
 
-    print('Updating all user documents', time.time())
-    usersCollection.bulk_write(results)
-    print('Updated all user documents', time.time())
+        print('Updating all user documents', time.time())
+        usersCollection.bulk_write(results)
+        print('Updated all user documents', time.time())
+    print('Ending', time.time())
 
-print('Ending', time.time())
+def add_empty_edits():
+    print('Starting', time.time())
+    usersCollection.update_many({ 'is_bot': False }, { '$set': { 'edits': [] } })
+    print('End', time.time())
+
+add_edits()
+add_empty_edits()
