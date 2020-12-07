4437a80bc92fd79e10d401ca233ec6416916e3f4
euberdeveloper <euberdeveloper@gmail.com>
2020-12-07 22:21:07 +0100

    feat: trial

---
+++ edits.py
@@ -0,0 +1,112 @@
+import pymongo
+import time
+
+client = pymongo.MongoClient()
+eventRevisionsCollection = client.wikimedia_history_it.revisions
+usersCollection = client.wikimedia_user_metrics.users
+
+print('Getting all non-bot users ids', time.time())
+idsResult = list(usersCollection.aggregate([
+    {
+        '$sort': 'id'
+    },
+    {
+        'limit': 10
+    },
+    {
+        '$match': {
+            'is_bot': False
+        }
+    },
+    {
+        '$group': {
+            '_id': None,
+            'ids': {
+                '$push': '$id'
+            }
+        }
+    }
+]))
+ids = idsResult[0].get('ids')
+print('Getted all non-bot users ids', time.time())
+
+print('Getting all data from revisions', time.time())
+results = list(eventRevisionsCollection.aggregate([
+    {
+        '$match': {
+            'event_user.id': {
+                '$in': ids
+            }
+        }
+    }, {
+        '$group': {
+            '_id': {
+                'id': '$event_user.id', 
+                'y': {
+                    '$year': '$event_timestamp'
+                }, 
+                'm': {
+                    '$month': '$event_timestamp'
+                }
+            }, 
+            'n': {
+                '$sum': 1
+            }
+        }
+    }, {
+        '$project': {
+            '_id': False, 
+            'id': '$_id.id', 
+            'v': [
+                [
+                    {
+                        '$concat': [
+                            {
+                                '$toString': '$_id.y'
+                            }, '/', {
+                                '$toString': '$_id.m'
+                            }
+                        ]
+                    }, '$n'
+                ]
+            ]
+        }
+    }, {
+        '$project': {
+            'id': 1, 
+            'v': {
+                '$arrayToObject': '$v'
+            }
+        }
+    }, {
+        '$group': {
+            '_id': '$id', 
+            'edits': {
+                '$mergeObjects': '$v'
+            }
+        }
+    }, {
+        '$project': {
+            'updateOne': {
+                'filter': {
+                    'id': '$_id'
+                }, 
+                'update': {
+                    'set': {
+                        'edits': '$edits'
+                    }
+                }
+            }
+        }
+    }
+]))
+print('Getted all data from revisions', time.time())
+
+print('Converting all data to update queries', time.time())
+for r in results:
+    r['updateOne']['update']['$set'] = r['updateOne']['update'].pop('$set')
+print('Converted all data to update queries', time.time())
+
+print('Updating all user documents', time.time())
+usersCollection.bulk_write(results)
+print('Updated all user documents', time.time())
