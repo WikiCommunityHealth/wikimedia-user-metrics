a8cbe600c6bdbf31899406fddefd65ec9905f12f
euberdeveloper <euberdeveloper@gmail.com>
2021-02-19 14:40:20 +0100

    feat: events per month
    Signed-off-by: euberdeveloper <euberdeveloper@gmail.com>

--- events_per_month.py
+++ events_per_month.py
@@ -4,14 +4,14 @@ import json
 
 client = pymongo.MongoClient()
 eventsCollectionPages = client.wikimedia_history_it.pages
-#eventsCollectionRevisions = client.wikimedia_history_it.revisions
+eventsCollectionRevisions = client.wikimedia_history_it.revisions
 
 
 def get_events_pages_per_month():
     print('Starting get_events_pages_per_month', time.time())
     result = [None] * 12
 
-    print('Getting parsed users', time.time())
+    print('Getting months info', time.time())
     monthsInfo = list(eventsCollectionPages.aggregate([
         {
             '$match': {
@@ -44,9 +44,48 @@ def get_events_pages_per_month():
     print('Done get_events_pages_per_month', time.time())
     return result
 
+def get_events_revisions_per_month():
+    print('Starting get_events_revisions_per_month', time.time())
+    result = [0] * 12
+
+    print('Getting months info', time.time())
+    monthsInfo = list(eventsCollectionRevisions.aggregate([
+        {
+            '$match': {
+                'event_user.is_bot_by': []
+            }
+        },
+        {
+            '$project': {
+                'month': {
+                    '$month': '$event_timestamp'
+                }
+            }
+        },
+        {
+            '$group': {
+                '_id': '$month',
+                'count': {
+                    '$sum': 1
+                }
+            }
+        }
+    ]))
+    print('Got months info', time.time())
+
+    print('Generating object', time.time())
+    for monthData in monthsInfo:
+        result[monthData['_id'] - 1] = monthData['count']
+    print('Generated object', time.time())
+
+    print('Done get_events_revisions_per_month', time.time())
+    return result
+
 
 events_pages_per_month = get_events_pages_per_month()
-text = json.dumps(events_pages_per_month)
+events_revisions_per_month = get_events_revisions_per_month()
+events_per_month = [x + y for x, y in zip(events_pages_per_month, events_revisions_per_month)]
+text = json.dumps(events_per_month)
 
-with open("events_pages_per_month.json", 'w') as outfile:
+with open("events_per_month.json", 'w') as outfile:
     outfile.write(text)
